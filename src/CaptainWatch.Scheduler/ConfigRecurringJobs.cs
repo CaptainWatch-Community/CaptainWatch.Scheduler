35283001abfef21a1775f23ed183d6e0d4adc9be
Killianlv <62741839+killianlv@users.noreply.github.com>
2025-03-06 21:43:46 +0100

    feat(scheduler): initialize project with Hangfire and add movies update wish count job

---
+++ src/CaptainWatch.Scheduler/ConfigRecurringJobs.cs
@@ -0,0 +1,23 @@
+﻿using CaptainWatch.Scheduler.Services.Movies;
+using Hangfire;
+
+namespace CaptainWatch.Scheduler
+{
+	public static class ConfigRecurringJobs
+	{
+		public static void ConfigureJobs()
+		{
+			//In this method we can configure all the recurring jobs that we need
+
+			RecurringJob.AddOrUpdate<MovieWriteService>(
+				"movies-update-wish-count",
+				service => service.UpdateWishCount(),
+				"0 1 * * *", //Every day at 1h00 UTC
+				new RecurringJobOptions
+				{
+					TimeZone = TimeZoneInfo.Utc
+				}
+			);
+		}
+	}
+}
---
+++ src/CaptainWatch.Scheduler/Services/Movies/MovieWriteService.cs
@@ -0,0 +1,23 @@
+﻿using CaptainWatch.API.ServiceClients;
+
+namespace CaptainWatch.Scheduler.Services.Movies
+{
+	public class MovieWriteService
+	{
+		#region Declarations
+
+		private readonly IMoviesWriteHttpClient _moviesWriteHttpClient;
+
+		public MovieWriteService(IMoviesWriteHttpClient moviesWriteHttpClient)
+		{
+			_moviesWriteHttpClient = moviesWriteHttpClient;
+		}
+
+		#endregion
+
+		public async Task UpdateWishCount()
+		{
+			await _moviesWriteHttpClient.UpdateWishCountAsync();
+		}
+	}
+}
